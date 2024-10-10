890103a075c55484e700187682488ce61dacd5f1
João Gabriel <joao.g.nascimento17@aluno.senai.br>
2024-10-10 12:38:19 -0300

    Introdução ao DOM

---
+++ aula_17/atividade-curriculo/atividade-curriculo.js
@@ -0,0 +1,8 @@
+const titulo = document.querySelector(".titulo")
+titulo.textContent = "Currículo"
+titulo.style.textAlign = "Center"
+titulo.style.fontSize = "60px"
+const subtitulo = document.querySelector(".subtitulo")
+subtitulo.textContent = "INFORMAÇÕES PESSOAIS"
+subtitulo.style.textDecoration = "bold"
+subtitulo.style.fontSize = "30px"
